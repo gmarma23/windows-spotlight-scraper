f9091fdcc2fdd6b9d4a6beb4123b7a16034bc633
gmarma23 <gmarma99@gmail.com>
2023-03-09 13:40:10 +0200

    Fixed typo

--- src/file_utils.py
+++ src/file_utils.py
@@ -6,7 +6,7 @@ USER_PICTURES_DIR = os.path.join(USER_ROOT_DIR, 'Pictures')
 USER_DOWNLOADS_DIR = os.path.join(USER_ROOT_DIR, 'Downloads')
 
 
-def create_dir_path(dirpath: str) -> None:
+def create_dirpath(dirpath: str) -> None:
     if not os.path.exists(dirpath):
         os.makedirs(dirpath)
 
