ec5d1e77ddc705619d1b7be2bd43113a607f6949
gmarma23 <gmarma99@gmail.com>
2023-03-09 13:28:07 +0200

    Minor improvements

--- src/file_utils.py
+++ src/file_utils.py
@@ -1,14 +1,18 @@
 import os
 
-USER_PICTURES_DIR = os.path.join(os.path.expanduser('~'), 'Pictures')
 
-# Utility to create provided path directories 
+USER_ROOT_DIR = os.path.expanduser('~')
+USER_PICTURES_DIR = os.path.join(USER_ROOT_DIR, 'Pictures')
+USER_DOWNLOADS_DIR = os.path.join(USER_ROOT_DIR, 'Downloads')
+
+
 def create_dir_path(dir_path: str) -> None:
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
 
-# Utility to remove invalid filename chars
+
 def sanitize_filename(filename: str) -> None:
     for ch in ['\\','/','<','>',':','\"','|','?','*']:
         if ch in filename:
             filename.replace(ch, '')
+            
