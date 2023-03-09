6a2dae2413ff478cbc2e8ee0be764ef20527ae5d
gmarma23 <gmarma99@gmail.com>
2023-03-09 23:23:27 +0200

    Configured app to run on terminal implemented arg parsing

---
+++ src/main.py
@@ -0,0 +1,30 @@
+from win_spotlight_scraper import WindowsSpotlightScraper
+import argparse
+
+
+def parse_args():
+    arg_parser = argparse.ArgumentParser(prog='Windows Spotlight Scraper')
+    arg_parser.add_argument(
+        '-c', '--count', 
+        action = 'store', 
+        type = int, 
+        required = False, 
+        help = 'Number of images to scrape'
+    )
+    return arg_parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    ws_scraper = WindowsSpotlightScraper()
+
+    print('')
+    if args.count:
+        ws_scraper.download_images(args.count)
+    else:
+        ws_scraper.download_images()
+    print('')
+
+
+if __name__ == '__main__':
+    main()
